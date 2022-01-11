8c4359a255a3b257075fd1ff480894e9885c8a0f
Islam Mohamed Ahmed Mohamed Abou Tabl <tot13_p@hotmail.com>
2022-01-11 13:22:41 +0200

    comments

--- wwwroot/setup.js
+++ wwwroot/setup.js
@@ -27,7 +27,7 @@ form.addEventListener('submit', function (e) {
         input.value = '';
     }
 });
-//join the room. Useful to count number of users in a room.
+//Join the room.
 socket.emit('joinroom', ev)
 socket.on(ev, function (msg) {
 
