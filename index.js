89fcee2f2eb9aebef3eb7c38ce4b51b4b7caef5c
Islam Mohamed Ahmed Mohamed Abou Tabl <tot13_p@hotmail.com>
2022-05-17 16:36:48 +0200

    support for files

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-const express = require('express');
+const express = require('express')
+const multer = require('multer')
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const helmet = require('helmet')
+const { uuid } = require('uuidv4');
+
 app.use(helmet())
 app.use(express.static('wwwroot'))
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, __dirname + '/wwwroot/uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, uuid() + '_' + file.originalname)
+  }
+})
+
+const upload = multer({ storage: storage })
+app.post('/upload', upload.array('files'), function (req, res, next) {
+  let filenames = req.files.map((file => file.filename))
+res.send(filenames)
+
+})
+
 io.on('connection', (socket) => {
   //New user connected
   socket.on('joinroom', (room) => {
@@ -18,12 +38,12 @@ io.on('connection', (socket) => {
       if (io.sockets.adapter.rooms.get(broad.ev).size > 2)
         socket.broadcast.emit(broad.ev, { type: -1 });
       else
-        socket.broadcast.emit(broad.ev, { type: broad.type, msg: broad.msg });
+        socket.broadcast.emit(broad.ev, { type: broad.type, msg: broad.msg, files: broad.files });
     }
     catch { }
   });
 });
 
 server.listen(process.env.PORT || 3000, () => {
-  
+
 });
--- wwwroot/setup.js
+++ wwwroot/setup.js
@@ -1,4 +1,9 @@
 ev = ""
+uploadUrl = location.href.split('?')[0]
+if (uploadUrl[uploadUrl.length - 1] == '/')
+    uploadUrl = uploadUrl.substring(0, uploadUrl.length - 1)
+uploadUrl = uploadUrl + '/upload'
+
 //parse query string to get room name if provided. if not provided, use default as the room name
 if (location.href.includes('?')) {
     var queryStr = location.href.split('?')[1]
@@ -15,29 +20,82 @@ var socket = io();
 
 var form = document.getElementById('form');
 var input = document.getElementById('txt');
+var filesElement = document.getElementById('files');
+var list = document.getElementById('list');
 //if 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-    if (input.value) {
+    if (filesElement.files.length > 0) {
+
+        let formData = new FormData()
+        for (let i = 0; i < filesElement.files.length; i++)
+            formData.append('files', filesElement.files[i])
+        axios({
+            method: "post",
+            url: uploadUrl,
+            data: formData
+        })
+            .then(function (response) {
+                //handle success
+                socket.emit('broadcast', {
+                    ev: ev,
+                    type: 1,
+                    msg: input.value,
+                    files: response.data
+                });
+                form.reset();
+                console.log(response);
+            })
+            .catch(function (response) {
+                //handle error
+                console.log(response);
+            });
+
+    }
+    else if (input.value) {
         socket.emit('broadcast', {
             ev: ev,
             type: 1,
-            msg: input.value
+            msg: input.value,
+            files: []
         });
-        input.value = '';
+        form.reset();
     }
 });
 //Join the room.
 socket.emit('joinroom', ev)
 socket.on(ev, function (msg) {
-
+    let firstMsg = true
+    let filesAfterLastMsg = false
     let payload = msg
     //payload.type = 1, means a message payload
+    let files = payload.files
     if (payload.type == 1) {
-        let txtarea = document.createElement("TEXTAREA");
-        txtarea.classList.add("form-control")
-        txtarea.value = msg.msg
-        document.getElementById('list').appendChild(txtarea)
+        if (msg.msg) {
+            if (!firstMsg || !filesAfterLastMsg) {
+                let br = document.createElement('BR')
+                list.appendChild(br)
+            }
+            let txtarea = document.createElement("TEXTAREA");
+            txtarea.readOnly = true
+            txtarea.classList.add("form-control")
+            txtarea.value = msg.msg
+            list.appendChild(txtarea)
+            firstMsg = false
+            filesAfterLastMsg = false
+        }
+        if (files) {
+            files.forEach(element => {
+                let anchor = document.createElement('A')
+                anchor.target = '_blank'
+                anchor.href = uploadUrl + 's/' + element
+                anchor.innerText = element
+                list.appendChild(anchor)
+                let br = document.createElement('BR')
+                list.appendChild(br)
+                filesAfterLastMsg = true
+            });
+        }
     }
     //Error payload
     else if (payload.type == -1) {
